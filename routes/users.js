710c0208c3e5e193eb0126a00c8eef842be81918
Ryan B <bombingrun3@gmail.com>
2025-05-13 22:55:42 -0500

    Fixed pathing issue in users.routers

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { getCurrentUser, updateCurrentUser } = require("../controllers/users");
 const auth = require("../middlewares/auth");
 
-router.get("/users/me", auth, getCurrentUser);
-router.patch("/users/me", auth, updateCurrentUser);
+router.get("/me", auth, getCurrentUser);
+router.patch("/me", auth, updateCurrentUser);
 
 module.exports = router;
