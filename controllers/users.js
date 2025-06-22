dab64ae3f412f8abccc7f727037a368c58e7a21f
Ryan B <bombingrun3@gmail.com>
2025-06-22 13:13:08 -0400

    Fixed more linting errors

--- controllers/users.js
+++ controllers/users.js
@@ -57,7 +57,7 @@ const createUser = (req, res, next) => {
       })
     )
     .then((user) => {
-      const { password, ...userWithoutPassword } = user.toObject();
+      const { password: removed, ...userWithoutPassword } = user.toObject();
       res.send(userWithoutPassword);
     })
     .catch((err) => {
