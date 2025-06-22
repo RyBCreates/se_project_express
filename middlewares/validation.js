e7bd911e01a9536bcb612a131df649498d8baf46
Ryan B <bombingrun3@gmail.com>
2025-06-22 15:24:44 -0400

    Added a few more error cases in the validation.js

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,9 +1,9 @@
 const { Joi, celebrate } = require("celebrate");
 const validator = require("validator");
 
-// Custom URL validator
+// Custom URL validator using validator.js
 const validateURL = (value, helpers) => {
-  if (validator.isURL(value)) {
+  if (validator.isURL(value, { require_protocol: true })) {
     return value;
   }
   return helpers.error("string.uri");
@@ -16,14 +16,17 @@ const validateCreateClothingItem = celebrate({
       "string.min": 'The minimum length of the "name" field is 2',
       "string.max": 'The maximum length of the "name" field is 30',
       "string.empty": 'The "name" field must be filled in',
+      "any.required": 'The "name" field is required',
     }),
     imageUrl: Joi.string().required().custom(validateURL).messages({
       "string.empty": 'The "imageUrl" field must be filled in',
       "string.uri": 'The "imageUrl" field must be a valid URL',
+      "any.required": 'The "imageUrl" field is required',
     }),
-    // weather: Joi.required().messages({
-    //   "checkbox.empty": 'The "weather" field is required',
-    // }),
+    weather: Joi.string().required().valid("hot", "warm", "cold").messages({
+      "any.required": 'The "weather" field is required',
+      "any.only": 'The "weather" field must be one of: hot, warm, cold',
+    }),
   }),
 });
 
@@ -34,16 +37,21 @@ const validateUserSignup = celebrate({
       "string.min": 'The minimum length of the "name" field is 2',
       "string.max": 'The maximum length of the "name" field is 30',
       "string.empty": 'The "name" field must be filled in',
+      "any.required": 'The "name" field is required',
     }),
     avatar: Joi.string().required().custom(validateURL).messages({
       "string.empty": 'The "avatar" field must be filled in',
       "string.uri": 'The "avatar" field must be a valid URL',
+      "any.required": 'The "avatar" field is required',
     }),
     email: Joi.string().required().email().messages({
       "string.empty": 'The "email" field must be filled in',
+      "any.required": 'The "email" field is required',
+      "string.email": 'The "email" field must be a valid email',
     }),
     password: Joi.string().required().messages({
       "string.empty": 'The "password" field must be filled in',
+      "any.required": 'The "password" field is required',
     }),
   }),
 });
@@ -53,9 +61,12 @@ const validateUserLogin = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email().messages({
       "string.empty": 'The "email" field must be filled in',
+      "any.required": 'The "email" field is required',
+      "string.email": 'The "email" field must be a valid email',
     }),
     password: Joi.string().required().messages({
       "string.empty": 'The "password" field must be filled in',
+      "any.required": 'The "password" field is required',
     }),
   }),
 });
@@ -63,9 +74,10 @@ const validateUserLogin = celebrate({
 // Validate item ID in route params
 const validateItemId = celebrate({
   params: Joi.object().keys({
-    itemId: Joi.string().hex().length(24).messages({
+    itemId: Joi.string().hex().length(24).required().messages({
       "string.hex": "Item ID is not the correct format",
-      "string.length": "Item ID is not the correct format",
+      "string.length": "Item ID must be 24 characters long",
+      "any.required": "Item ID is required",
     }),
   }),
 });
