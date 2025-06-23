fb1631c5de9eae80372c06fa9c8280e3301bf91f
Ryan B <bombingrun3@gmail.com>
2025-06-23 17:01:18 -0400

    Added Enviornment variables

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
+const { JWT_SECRET = "super-strong-secret" } = process.env;
+
 module.exports = {
-  JWT_SECRET:
-    process.env.JWT_SECRET ||
-    "cbf552339e51d97ff90451bbd396da1a457da26ae3ee7d985828979d210fb7",
+  JWT_SECRET,
 };
